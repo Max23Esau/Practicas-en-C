5496e4dd95e0c5830b5dc8e71f2a103ced5f8611
Esau Maximino <esa-max23@live.com>
2019-11-27 18:01:40 -0600

    Create conversorDigital.c

---
+++ conversorDigital.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+void pesos_a_dolares()
+{
+  float resultado, cantidad, dolar = 19.56;
+
+  printf("\nIngresa la cantidad que deseas convertir: ");
+  scanf("%f", &cantidad);
+  resultado = cantidad * dolar;
+  printf("%.2f pesos son %.2f dolares", cantidad, resultado);
+}
+void dolares_a_pesos()
+{
+  float resultado, cantidad, peso = 0.051;
+  printf("\nIngresa la cantidad que deseas convertir: ");
+  scanf("%f", &cantidad);
+  resultado = cantidad * peso ;
+  printf("%.2f dolares son %.2f pesos", cantidad, resultado);
+}
+
+int main(void) {
+
+  int option;
+
+  printf("\nConversor de divisas\n");
+  printf("\n1.Pesos a Dolares");
+  printf("\n2.Dolares a Pesos");
+  printf("\nQue opcion elige?: ");
+  scanf("%i", &option);
+
+  if(option == 1)
+  {
+    pesos_a_dolares(); 
+  }
+  else if(option == 2)
+  {
+    dolares_a_pesos();
+  }
+  else
+  {
+    printf("Opcion no valida");
+  }
+
+  return 0;
+}
